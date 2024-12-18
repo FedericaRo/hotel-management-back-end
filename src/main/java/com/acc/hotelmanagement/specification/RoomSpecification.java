474025a6166c9ad99f4de29b47004e425f3f822c
Federica Rocchetti <rocchetti.federica@gmail.com>
2024-12-18 11:16:18 +0100

    Fix dynamic room filter using JPA Specifications
    - Changed query method to exclude rooms with overlapping bookings
    - Previous method incorrectly returned rooms that had both overlapping and non overlapping bookings

--- src/main/java/com/acc/hotelmanagement/specification/RoomSpecification.java
+++ src/main/java/com/acc/hotelmanagement/specification/RoomSpecification.java
@@ -4,6 +4,8 @@ import com.acc.hotelmanagement.model.Booking;
 import com.acc.hotelmanagement.model.Room;
 import jakarta.persistence.criteria.Join;
 import jakarta.persistence.criteria.JoinType;
+import jakarta.persistence.criteria.Root;
+import jakarta.persistence.criteria.Subquery;
 import org.springframework.data.jpa.domain.Specification;
 
 import java.time.LocalDate;
@@ -11,6 +13,7 @@ import java.time.LocalDate;
 //https://docs.spring.io/spring-data/jpa/reference/jpa/specifications.html#page-title
 // https://spring.io/blog/2011/04/26/advanced-spring-data-jpa-specifications-and-querydsl
 // https://github.com/AhmetAksunger/Jpa-Specifications-Example
+//https://vladmihalcea.com/exists-subqueries-jpa-hibernate/
 public class RoomSpecification {
 
     public static Specification<Room> withNumberOfGuests(Integer numberOfGuests) {
@@ -31,22 +34,52 @@ public class RoomSpecification {
             if (checkIn == null || checkOut == null)
                 return criteriaBuilder.conjunction(); // returns all rooms if check in or checkout filter is null
 
-            Join<Room, Booking> roomJoin = root.join("bookings", JoinType.LEFT); // use left join to get all rooms even if they have no bookings
-            return // true --> room is available and included, false --> room is not available and not included
-                    criteriaBuilder.or(
-                            roomJoin.isNull(),// if no booking exists, room is available
-                            criteriaBuilder.not( // invert the boolean: is overlapping? no -> so put it in the result set
-                                    criteriaBuilder.and( // is overlapping? FALSE
-                                            criteriaBuilder.lessThan(roomJoin.get("checkInDate"), checkOut),
-                                            criteriaBuilder.greaterThan(roomJoin.get("checkOutDate"), checkIn)
-                                    )
-                            )
+            // Create a subquery to check for overlapping bookings
+            Subquery<Integer> subquery = query.subquery(Integer.class);
+            Root<Booking> bookingRoot = subquery.from(Booking.class); //represents the bookings table
+
+            // Selects a literal value to indicate the existence of matching rows, without returning specific rows from bookings
+            // if it returns at least one row it means that there is an overlapping booking
+            subquery.select(criteriaBuilder.literal(1))
+            //  subquery.select(bookingRoot.get("id"))
+                    .where(
+                            // links the subquery bookings to the main root room by matching room.id to the booking.room_id
+                            criteriaBuilder.equal(bookingRoot.get("room").get("id"), root.get("id")),
+                            // check if booking overlaps
+                            criteriaBuilder.lessThan(bookingRoot.get("checkInDate"), checkOut), // checkin before requested checkout
+                            criteriaBuilder.greaterThan(bookingRoot.get("checkOutDate"), checkIn) // and checkout after requested checkin
                     );
+            // exists checks if the subquery returns at least one row
+            // not -> ensures to get rooms with no overlapping bookings
+            return criteriaBuilder.not(criteriaBuilder.exists(subquery));
+
         };
     }
+// SQL query
+//    select
+//    r1_0.id,
+//    r1_0.number_of_guests,
+//    r1_0.price,
+//    r1_0.type
+//            from
+//    rooms r1_0
+//    where
+//    r1_0.number_of_guests=?
+//    and r1_0.type=?
+//    and not exists(select
+//            1
+//                    from
+//                    bookings b1_0
+//                    where
+//                    b1_0.room_id=r1_0.id
+//                    and b1_0.check_in_date<? (checkout)
+//                    and b1_0.check_out_date>? (checkin) )
 
-    // versione con moltissimi commenti esplicativi sul flow delle condizioni booleane
-//    public static Specification<Room> isAvailableBetween(LocalDate checkIn, LocalDate checkOut) {
+
+    // // questa versione crea problemi quando ci sono più di un booking per stanza alcuni che si sovrappongono alcuni no, la stanza viene ecomunque inserita
+// //  left join valuta ogni prenotazione independentemente , quindi se c'è una prenotazione per quella stanza che non si sovrappone viene inserita, anche se ce n'è una che si sovrapone
+// // versione con moltissimi commenti esplicativi sul flow delle condizioni booleane
+//    public static Specification<Room> isAvailableBetween2(LocalDate checkIn, LocalDate checkOut) {
 //        return (root, query, criteriaBuilder) ->
 //        {
 //            if (checkIn == null || checkOut == null)
@@ -72,5 +105,24 @@ public class RoomSpecification {
 //                    );
 //        };
 //    }
+
+//    select
+//    r1_0.id,
+//    r1_0.number_of_guests,
+//    r1_0.price,
+//    r1_0.type
+//            from
+//    rooms r1_0
+//    left join
+//    bookings b1_0
+//    on r1_0.id=b1_0.room_id
+//            where
+//    r1_0.number_of_guests=?
+//    and r1_0.type=?
+//    and (
+//            b1_0.id is null
+//                    or not(b1_0.check_in_date<?
+//                    and b1_0.check_out_date>?)
+//        )
 }
 
